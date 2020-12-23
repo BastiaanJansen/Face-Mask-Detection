fbc50222167508c48f20d6ffdb67ee3a79dc2142
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-23 12:55:16 +0100

    Fixed bug

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -95,13 +95,14 @@ print("[INFO] Starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
+
 def draw_roi(frame, box, prediction):
     # unpack the bounding box and predictions
     (start_x, start_y, end_x, end_y) = box
     (mask, withoutMask) = prediction
     # determine the class label and color we'll use to draw the bounding box and text
     label = "Mask" if mask > withoutMask else "No Mask"
-    color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+    color = (0, 255, 0) if label == "Mask" else (52, 173, 72)
     # include the probability in the label
     label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
@@ -112,9 +113,12 @@ def draw_roi(frame, box, prediction):
     # display the label and bounding box rectangle on the output frame
     (text_width, text_height) = cv2.getTextSize(label, font, fontScale=font_scale, thickness=1)[0]
 
-    cv2.rectangle(frame, (start_x, start_y - text_height - (text_padding * 2)), (start_x + text_width + (text_padding * 2), start_y), color, -1)
-    cv2.putText(frame, label, (start_x + text_padding, start_y - text_padding), font, font_scale, (255, 255, 255, 255), 1)
+    cv2.rectangle(frame, (start_x - 1, start_y - text_height - (text_padding * 2)),
+                  (start_x + text_width + (text_padding * 2), start_y), color, -1)
+    cv2.putText(frame, label, (start_x + text_padding, start_y - text_padding), font, font_scale, (255, 255, 255, 255),
+                1)
     cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color, 2)
+
 
 # loop over the frames from the video stream
 while True:
