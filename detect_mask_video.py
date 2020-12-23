4f83fb2837bbda8c5e09197fe3ac13a47de24d9d
moostoet <moostoet@hotmail.com>
2020-12-23 13:02:01 +0100

    made comments look nicer

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -16,10 +16,10 @@ MIN_CONFIDENCE = 0.50
 def extract_face_roi(frame, detection):
     h, w = frame.shape[:2]
 
-    # compute the (x, y)-coordinates of the bounding box for the object
+    # Compute the (x, y)-coordinates of the bounding box for the object
     box = detection * np.array([w, h, w, h])
     start_x, start_y, end_x, end_y = box.astype("int")
-    # ensure the bounding boxes fall within the dimensions of the frame
+    # Ensure the bounding boxes fall within the dimensions of the frame
     start_x, start_y = (max(0, start_x), max(0, start_y))
     end_x, end_y = (min(w - 1, end_x), min(h - 1, end_y))
 
@@ -34,27 +34,27 @@ def extract_face_roi(frame, detection):
 
 
 def detect_faces(frame, face_net):
-    # grab the dimensions of the frame and then construct a blob from it
+    # Grab the dimensions of the frame and then construct a blob from it
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the face detections
+    # Pass the blob through the network and obtain the face detections
     face_net.setInput(blob)
     detections = face_net.forward()
 
-    # initialize our list of faces, their corresponding locations,
+    # Initialize our list of faces, their corresponding locations,
     # and the list of predictions from our face mask network
     faces = []
     locations = []
 
     # loop over the detections
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the detection
+        # Extract the confidence (i.e., probability) associated with the detection
         confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the confidence is greater than the minimum confidence
+        # Filter out weak detections by ensuring the confidence is greater than the minimum confidence
         if confidence > MIN_CONFIDENCE:
             face, start_x, start_y, end_x, end_y = extract_face_roi(frame, detections[0, 0, i, 3:7])
-            # add the face and bounding boxes to their respective lists
+            # Add the face and bounding boxes to their respective lists
             faces.append(face)
             locations.append((start_x, start_y, end_x, end_y))
 
@@ -64,15 +64,15 @@ def detect_faces(frame, face_net):
 def predict_mask(faces, mask_net):
     predictions = []
 
-    # only make a predictions if at least one face was detected
+    # Only make a predictions IF at least one face was detected
     if len(faces) > 0:
-        # for faster inference we'll make batch predictions on *all*
+        # For faster inference we'll make batch predictions on *all*
         # faces at the same time rather than one-by-one predictions
         # in the above `for` loop
         faces = np.array(faces, dtype="float32")
         predictions = mask_net.predict(faces, batch_size=32)
 
-    # return a 2-tuple of the face locations and their corresponding locations
+    # Return a 2-tuple of the face locations and their corresponding locations
     return predictions
 
 
@@ -80,37 +80,37 @@ if not os.path.exists(MODEL):
     print("[INFO] Model does not exist")
     exit(1)
 
-# load our serialized face detector model from disk
+# Load our serialized face detector model from disk
 print("[INFO] Loading face detector model...")
 prototxtPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
 weightsPath = os.path.sep.join(["face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
 face_net = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
+# Load the face mask detector model from disk
 print("[INFO] Loading face mask detector model...")
 mask_net = load_model(MODEL)
 
-# initialize the video stream and allow the camera sensor to warm up
+# Initialize the video stream and allow the camera sensor to warm up
 print("[INFO] Starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
 
 def draw_roi(frame, box, prediction):
-    # unpack the bounding box and predictions
+    # Unpack the bounding box and predictions
     (start_x, start_y, end_x, end_y) = box
     (mask, withoutMask) = prediction
-    # determine the class label and color we'll use to draw the bounding box and text
+    # Determine the class label and color we'll use to draw the bounding box and text
     label = "Mask" if mask > withoutMask else "No Mask"
     color = (52, 173, 72) if label == "Mask" else (0, 0, 255)
-    # include the probability in the label
+    # Include the probability in the label
     label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
     font_scale = 0.45
     font = cv2.FONT_HERSHEY_SIMPLEX
     text_padding = 10
 
-    # display the label and bounding box rectangle on the output frame
+    # Display the label and bounding box rectangle on the output frame
     (text_width, text_height) = cv2.getTextSize(label, font, fontScale=font_scale, thickness=1)[0]
 
     cv2.rectangle(frame, (start_x - 1, start_y - text_height - (text_padding * 2)),
@@ -120,20 +120,19 @@ def draw_roi(frame, box, prediction):
     cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color, 2)
 
 
-# loop over the frames from the video stream
+# Loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
+    # Grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
     frame = vs.read()
-    # detect faces in the frame and determine if they are wearing a face mask or not
+    # Detect faces in the frame and determine if they are wearing a face mask or not
     faces, locations = detect_faces(frame, face_net)
     predictions = predict_mask(faces, mask_net)
 
-    # loop over the detected face locations and their corresponding
-    # locations
+    # Loop over the detected face locations and their corresponding locations
     for (box, pred) in zip(locations, predictions):
         draw_roi(frame, box, pred)
 
-    # show the output frame
+    # Show the output frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
