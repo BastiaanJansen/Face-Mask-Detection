74b03cba2af2cf574f932234c539f43150cc7ab1
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-23 12:25:45 +0100

    Created extract face roi function

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -13,6 +13,26 @@ MODEL = "models/mask_detector.model"
 MIN_CONFIDENCE = 0.50
 
 
+def extract_face_ROI(frame, detection):
+    h, w = frame.shape[:2]
+
+    # compute the (x, y)-coordinates of the bounding box for the object
+    box = detection * np.array([w, h, w, h])
+    start_x, start_y, end_x, end_y = box.astype("int")
+    # ensure the bounding boxes fall within the dimensions of the frame
+    start_x, start_y = (max(0, start_x), max(0, start_y))
+    end_x, end_y = (min(w - 1, end_x), min(h - 1, end_y))
+
+    # Extract the face ROI, convert it from BGR to RGB channel ordering, resize it to 224x224, and preprocess it
+    face = frame[start_y: end_y, start_x: end_x]
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+    face = cv2.resize(face, (224, 224))
+    face = img_to_array(face)
+    face = preprocess_input(face)
+
+    return face, start_x, start_y, end_x, end_y
+
+
 def detect_faces(frame, face_net):
     # grab the dimensions of the frame and then construct a blob
     # from it
@@ -30,32 +50,14 @@ def detect_faces(frame, face_net):
 
     # loop over the detections
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
+        # extract the confidence (i.e., probability) associated with the detection
         confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
+        # filter out weak detections by ensuring the confidence is greater than the minimum confidence
         if confidence > MIN_CONFIDENCE:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
-            face = frame[startY:endY, startX:endX]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face, (224, 224))
-            face = img_to_array(face)
-            face = preprocess_input(face)
-            # add the face and bounding boxes to their respective
-            # lists
+            face, start_x, start_y, end_x, end_y = extract_face_ROI(frame, detections[0, 0, i, 3:7])
+            # add the face and bounding boxes to their respective lists
             faces.append(face)
-            locations.append((startX, startY, endX, endY))
+            locations.append((start_x, start_y, end_x, end_y))
 
     return faces, locations
 
@@ -96,12 +98,9 @@ time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
+    # grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
     frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-    # detect faces in the frame and determine if they are wearing a
-    # face mask or not
+    # detect faces in the frame and determine if they are wearing a face mask or not
     faces, locations = detect_faces(frame, face_net)
     predictions = predict_mask(faces, mask_net)
 
