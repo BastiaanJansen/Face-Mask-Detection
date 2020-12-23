ded2fe60065ccc1c5770461266e56f562a72c356
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-23 12:56:15 +0100

    Bugfix

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -102,7 +102,7 @@ def draw_roi(frame, box, prediction):
     (mask, withoutMask) = prediction
     # determine the class label and color we'll use to draw the bounding box and text
     label = "Mask" if mask > withoutMask else "No Mask"
-    color = (0, 255, 0) if label == "Mask" else (52, 173, 72)
+    color = (52, 173, 72) if label == "Mask" else (0, 0, 255)
     # include the probability in the label
     label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
